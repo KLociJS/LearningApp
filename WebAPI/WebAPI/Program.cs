50077d1186997bbe425b412256d6a73266210e4b
KLociJS <kisznerlorant21@gmail.com>
2023-07-01 11:31:49 +0200

    chore: configure program.cs for identity

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Contexts;
+using WebAPI.Models;
+using WebAPI.SeedData;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>( options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DataContext")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>()
+    .AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDataContext>();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,8 +31,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Seed roles and admin user 
+// using (var scope = app.Services.CreateScope())
+// {
+//     var services = scope.ServiceProvider;
+//     
+//     var context = services.GetRequiredService<AppDataContext>();
+//     context.Database.Migrate();
+//
+//     var userManager = services.GetRequiredService<UserManager<AppUser>>();
+//     var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+//
+//     await SeedData.Init(userManager, roleManager, context);
+// }
 
 app.Run();
