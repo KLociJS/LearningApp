d21355ed54c201db1f8c0b3e42070de6b1805e7e
KLociJS <kisznerlorant21@gmail.com>
2023-07-04 20:27:47 +0200

    feat: Add reset password page

---
+++ client/src/Pages/ResetPassword/ResetPassword.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+import InputLabel from '../../Components/Input'
+
+import { FiLock } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+export default function ResetPassword() {
+
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
+
+  const handleLogin = () => {
+
+  }
+
+  return (
+    <>
+    <main className='container card-container'>
+      <div className='card'>
+        <FiLock className='card-icon mb-1' />
+        <h2 className='heading-2'>Reset Password</h2>
+        <InputLabel label='Email address' type='email' inputValue={email} setInputValue={setEmail} setError={setError}/>
+        {error && <p>{error}</p>}
+        <Link to='/login' className='link align-end'>Back to login</Link>
+        <button className='primary-button mt-2' onClick={handleLogin}>Send request</button>
+      </div>
+    </main>
+  </>
+  )
+}
