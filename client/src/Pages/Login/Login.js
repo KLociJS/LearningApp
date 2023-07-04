95d8d7fa5a28282c2fba1ce776eb0c1a2f449508
KLociJS <kisznerlorant21@gmail.com>
2023-07-04 12:41:31 +0200

    refactor: Improve style structure

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import './Login.css'
 import InputLabel from '../../Components/Input/InpuLabel'
 
 import { AiOutlineLogin } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   const [userName, setUserName] = useState('');
@@ -18,10 +19,11 @@ export default function Login() {
       <main className='container card-container'>
         <div className='card'>
           <AiOutlineLogin className='card-icon' />
-          <h2 className='card-heading'>Login</h2>
+          <h2 className='heading-1'>Login</h2>
           <InputLabel label='User name' inputValue={userName} setInputValue={setUserName}/>
-          <InputLabel label='Password' inputValue={password} setInputValue={setPassword}/>  
-          <button className='primary-button'>Login</button>
+          <InputLabel label='Password' inputValue={password} setInputValue={setPassword}/>
+          <Link to='/password-reset' className='link align-end mt-1'>Forgot password?</Link>
+          <button className='primary-button mt-2' onClick={handleLogin}>Login</button>
         </div>
       </main>
     </>
