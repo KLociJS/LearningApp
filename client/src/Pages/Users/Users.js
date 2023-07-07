7456b1e21faaf9213956f8b95b98cf0cfc9c999c
KLociJS <kisznerlorant21@gmail.com>
2023-07-07 14:02:23 +0200

    feat: Add roles modal for changing roles

--- client/src/Pages/Users/Users.js
+++ client/src/Pages/Users/Users.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 import './Users.css'
+import Modal from '../../Components/Modal/Modal'
+import RolesModalContent from './RolesModal/RolesModalContent'
 
 export default function Users() {
 
@@ -51,7 +53,6 @@ export default function Users() {
                         <th className='table-heading'>Roles</th>
                         <th className='table-heading'>Email</th>
                         <th className='table-heading'></th>
-                        <th className='table-heading'></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -60,8 +61,14 @@ export default function Users() {
                         <td className='table-data'>{u.userName}</td>
                         <td className='table-data'>{u.roles.join(', ')}</td>
                         <td className='table-data'>{u.email}</td>
-                        <td className='table-data'>Change roles</td>
-                        <td className='table-data'><button onClick={()=>handleUserDelete(u.id)}>Delete</button></td>
+                        <td className='table-data'>
+                          <Modal modalButtonText='Change role'>
+                            <RolesModalContent user={u} setUsers={setUsers} />
+                          </Modal>
+                          <button onClick={()=>handleUserDelete(u.id)} className='warning-button ml-2'>
+                            Delete
+                          </button>
+                        </td>
                     </tr>))}
                 </tbody>
             </table>
