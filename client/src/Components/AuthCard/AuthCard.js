e39f7ac1c3d346dbe5f161e95fca396cbb2396b3
KLociJS <kisznerlorant21@gmail.com>
2023-07-10 11:28:08 +0200

    fix: Disable browser validation on authforms

--- client/src/Components/AuthCard/AuthCard.js
+++ client/src/Components/AuthCard/AuthCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function AuthCard({ icon: Icon, heading, children, onSubmit }) {
   return (
     <main className="container card-container">
-      <form className="card" onSubmit={onSubmit}>
+      <form className="card" onSubmit={onSubmit} noValidate>
         {Icon && <Icon className="card-icon" />}
         {heading && <h2 className="heading-1">{heading}</h2>}
         {children}
