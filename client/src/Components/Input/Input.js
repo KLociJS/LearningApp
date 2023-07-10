42471477513cf1f4628c91df79186553c276364c
KLociJS <kisznerlorant21@gmail.com>
2023-07-10 11:22:35 +0200

    refactor: fix input field component

---
+++ client/src/Components/Input/Input.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './InputField.css';
+
+const Input = ({ 
+  label, 
+  inputValue, 
+  setInputValue,
+  hasError,
+  type, 
+}) => {
+  const [isFocused, setIsFocused] = useState(false)
+
+  const handleFocus = () => setIsFocused(true)
+  const handleBlur = () => setIsFocused(false)
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  };
+
+  const isActive = isFocused || inputValue ? 'active' : ''
+
+  return (
+        <div className={`input-group ${hasError ? 'error' : ''}`}>
+            <label className={`input-label ${isActive}`}>
+                {label}
+            </label>
+            <input
+                type={type || 'text'}
+                value={inputValue}
+                className="input-control"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                autoComplete="off"
+            />
+    </div>
+  );
+};
+
+export default Input
