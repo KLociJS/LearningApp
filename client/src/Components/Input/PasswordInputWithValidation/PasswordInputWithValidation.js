59fce22068e7a06c460ddda1be6fe3ff9d14bd00
KLociJS <kisznerlorant21@gmail.com>
2023-07-10 11:24:34 +0200

    refactor: Add PasswordInputWithValidation component

---
+++ client/src/Components/Input/PasswordInputWithValidation/PasswordInputWithValidation.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import Input from '../Input'
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
+
+export default function PasswordInputWithValidation({ inputValue, setInputValue, error, setError }) {
+    const [showPassword, setShowPassword] = useState(false)
+
+    useEffect(()=>{
+        setError('')
+    },[inputValue,setError])
+    
+  return (
+    <>
+        <div className='password-input'>
+            <Input 
+                type={showPassword ? 'text' : 'password'}
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+                hasError={error}
+                label='Password'
+            />
+            {showPassword ? 
+                <AiOutlineEye 
+                className='password-icon'
+                onClick={()=>setShowPassword(false)}
+                /> :
+                <AiOutlineEyeInvisible
+                className='password-icon'
+                onClick={()=>setShowPassword(true)}
+                />
+            }
+        </div>
+        {error && <p className='error-msg align-start'>{error}</p>}
+    </>
+  )
+}
