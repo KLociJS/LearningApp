f08278b852a398a6a518efda760c5b651c8287ff
KLociJS <kisznerlorant21@gmail.com>
2023-07-10 11:27:18 +0200

    refactor:  Modify components exports

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,18 +1,26 @@
-import InputField from "./Input/InputField"
+import Input from "./Input/Input"
 import Modal from "./Modal/Modal"
 import AuthCard from "./AuthCard/AuthCard"
 import RequireRoles from "./RequireRoles/RequireRoles"
 import UnauthenticatedRoute from "./UnauthenticatedRoute/UnauthenticatedRoute"
 import RoleBasedRender from "./RoleBasedRender/RoleBasedRender"
 import AuthBasedRender from "./AuthBasedRender/AuthBasedRender"
+import PasswordInputWithValidation from "./Input/PasswordInputWithValidation/PasswordInputWithValidation"
+import PasswordInput from "./Input/PasswordInput/Passwordinput"
+import EmailInputWithValidation from "./Input/EmailInputWithValidation/EmailInputWithValidation"
+import UserNameInputWithValidation from "./Input/UserNameInputWithValidation/UserNameInputWithValidation"
 
 
 export { 
-    InputField,
+    Input,
     Modal,
     AuthCard,
     RequireRoles,
     UnauthenticatedRoute,
     RoleBasedRender,
     AuthBasedRender,
+    PasswordInputWithValidation,
+    EmailInputWithValidation,
+    UserNameInputWithValidation,
+    PasswordInput,
 }
