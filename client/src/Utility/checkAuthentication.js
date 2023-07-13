6d168354905ade2c201f4161f86b49e82bbd06cc
KLociJS <kisznerlorant21@gmail.com>
2023-07-13 16:41:26 +0200

    refactor: Improve check-auth in react

--- client/src/Utility/checkAuthentication.js
+++ client/src/Utility/checkAuthentication.js
@@ -2,9 +2,13 @@ export default function checkAuthentication(setUser,setIsAuthenticationDone) {
     fetch('https://localhost:7120/api/Auth/check-authentication', {
       credentials: 'include'
     })
-    .then(res => res.json())
     .then(res => {
-      if(!res.unAuthorized){
+      if(res.ok){
+        return res.json()
+      }
+    })
+    .then(res => {
+      if(res){
         setUser(res)
       }
       setIsAuthenticationDone(true)
