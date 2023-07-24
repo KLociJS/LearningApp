32a56e9b73b86aa47912827759fc31dedbc2121f
KLociJS <kisznerlorant21@gmail.com>
2023-07-24 13:53:33 +0200

    refactor: Decouple user service logic from controller

--- WebAPI/WebAPI/Services/IUserService.cs
+++ WebAPI/WebAPI/Services/IUserService.cs
@@ -1,15 +1,12 @@
-using WebAPI.Models;
 using WebAPI.Models.RequestDtos;
+using WebAPI.Models.ResponseDto;
 using WebAPI.Models.ResultModels;
 
 namespace WebAPI.Services;
 
 public interface IUserService
 {
-    Task<RegisterResult> RegisterUserAsync(RegisterUserDto registerUserDto);
-    Task<ConfirmEmailResult> ConfirmEmailAsync(string email, string token);
-    Task<LoginResult> LoginAsync(LoginUserDto loginUserDto);
-    Task<RequestPasswordChangeResult> RequestPasswordChangeAsync(string email);
-    Task<ChangeForgotPasswordResult> ChangeForgotPasswordAsync(ResetPasswordDto resetPasswordDto);
-    Task<IList<string>> GetRolesAsync(string userName);
+    Task<List<UserDto>> GetUsers();
+    Task<DeleteUserResult> DeleteUserById(string id);
+    Task<ChangeRolesResult> ChangeRole(string userId, UserRolesDto rolesDto);
 }
