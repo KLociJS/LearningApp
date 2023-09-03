287532fef9b080b5f6b959c1b7e8bfbb68e8e249
Lóránt Kiszner <100793707+KLociJS@users.noreply.github.com>
2023-09-03 21:36:55 +0200

    Revert "Sidebar for browsing notes"

--- client/src/Pages/_Layout/Layout.js
+++ client/src/Pages/_Layout/Layout.js
@@ -8,7 +8,6 @@ export default function Layout() {
     <>
       <NavBar />
       <Outlet />
-      <div className='footer'></div>
     </>
   )
 }
--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,13 +1,12 @@
-import Layout from "./_Layout/Layout"
-import Home from "./Home/Home"
-import Login from "./Login/Login"
-import ResetPassword from "./ResetPassword/ResetPassword"
-import SingUp from "./SingUp/SignUp"
-import Users from "./Users/Users"
-import UnAuthorized from "./UnAuthorized/UnAuthorized"
-import ConfirmEmail from "./ConfirmEmail/ConfirmEmail"
-import ForgotPassword from "./ForgotPassword/ForgotPassword"
-import Articles from "./Articles/Articles"
+import Layout from "./_Layout/Layout";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import ResetPassword from "./ResetPassword/ResetPassword";
+import SingUp from "./SingUp/SignUp";
+import Users from "./Users/Users";
+import UnAuthorized from "./UnAuthorized/UnAuthorized";
+import ConfirmEmail from "./ConfirmEmail/ConfirmEmail";
+import ForgotPassword from "./ForgotPassword/ForgotPassword";
 
 export {
     Layout,
@@ -19,5 +18,4 @@ export {
     UnAuthorized,
     ConfirmEmail,
     ForgotPassword,
-    Articles,
 }
