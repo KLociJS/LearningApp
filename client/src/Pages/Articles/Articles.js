168dddb69dd99239d551c688d53e34e6af1fb2ce
KLociJS <kisznerlorant21@gmail.com>
2023-09-05 10:26:12 +0200

    refactor: Move sidebar fetch into sidebar component

--- client/src/Pages/Articles/Articles.js
+++ client/src/Pages/Articles/Articles.js
@@ -3,35 +3,16 @@ import { Outlet, useParams } from 'react-router-dom'
 import './Articles.css'
 
 import Sidebar from './Components/Sidebar/Sidebar'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 
 export default function Articles() {
     const { id } = useParams()
     const [article,setArticle] = useState([])
-    const [sidebarContent, setSidebarContent] = useState()
-
-    useEffect(()=>{
-      fetch('http://localhost:5000/api/Article',{
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: 'include'
-        })
-        .then(res=>res.json())
-        .then(data=>{
-          console.log(data)
-          setSidebarContent(data)
-        })
-        .catch(err=>console.log(err))
-    },[])
-
-    if(sidebarContent === undefined) return null
 
     return (
       <>
-        <Sidebar content={sidebarContent} />
+        <Sidebar />
         <section className='article-container'>
             <Outlet context={{article}}/>
         </section>
--- client/src/Pages/Articles/Components/Sidebar/Sidebar.js
+++ client/src/Pages/Articles/Components/Sidebar/Sidebar.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
+import { getSidebarContentUrl } from '_Constants/fetchUrl'
+
 import ArtilceLink from './ArticleLink/ArtilceLink'
 import Category from './Category/Category'
 
 import { MdOutlinePostAdd } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
-export default function Sidebar({ content }) {
-  console.log(content)
+export default function Sidebar() {
+  const [sidebarContent, setSidebarContent] = useState(null)
+
+    useEffect(()=>{
+      fetch( getSidebarContentUrl ,{
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+          console.log(data)
+          setSidebarContent(data)
+        })
+        .catch(err=>console.log(err))
+    },[])
+
   return (
     <aside className='sidebar'>
         <Link to='/create-article' className='create-article'>
@@ -14,8 +34,8 @@ export default function Sidebar({ content }) {
           <MdOutlinePostAdd  className='add-icon'/>
         </Link>
         <ul className='category-list'>
-            {content.categories.map(c=>(<Category key={c.id} category={c} />))}
-            <ArtilceLink articles={content.articles} />
+            {sidebarContent && sidebarContent.categories.map(c=>(<Category key={c.id} category={c} />))}
+            <ArtilceLink articles={sidebarContent?.articles || []} />
         </ul>
     </aside>
   )
