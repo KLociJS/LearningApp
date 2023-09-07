a7baf6b95ec4712019946cc28efdcc3ecda80eee
KLociJS <kisznerlorant21@gmail.com>
2023-09-07 22:57:19 +0200

    style: Implement mobile/tablet sidebar

--- client/src/Pages/Articles/Components/Sidebar/Sidebar.js
+++ client/src/Pages/Articles/Components/Sidebar/Sidebar.js
@@ -1,25 +1,39 @@
-
 import ArtilceLink from './ArticleLink/ArtilceLink'
 import Category from './Category/Category'
 import SidebarSkeleton from './SidebarSkeleton/SidebarSkeleton'
 
-import { MdOutlinePostAdd } from 'react-icons/md'
+import './Sidebar.css'
+
+import { MdKeyboardDoubleArrowRight, MdKeyboardDoubleArrowLeft, MdOutlinePostAdd } from 'react-icons/md'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Sidebar({sidebarContent, isLoading}) {
+
+  const [isOpen, setIsOpen] = useState(true)
+
   if(isLoading){
     return <SidebarSkeleton />
   }
+
   return (
-    <aside className='sidebar'>
-        <Link to='/create-article' className='create-article'>
-          New Note
-          <MdOutlinePostAdd  className='add-icon'/>
-        </Link>
-        <ul className='category-list'>
-            {sidebarContent && sidebarContent.categories.map(c=>(<Category key={c.id} category={c} />))}
-            <ArtilceLink articles={sidebarContent?.articles || []} />
-        </ul>
-    </aside>
+    <>
+      <aside className={`sidebar ${isOpen ? 'active' : ''}`}>
+      <div className={`sidebar-icon-container ${isOpen ? 'active' : ''}`}>
+        {isOpen ? 
+        <MdKeyboardDoubleArrowLeft className='sidebar-icon' onClick={()=>setIsOpen(false)}/> :
+        <MdKeyboardDoubleArrowRight className='sidebar-icon' onClick={()=>setIsOpen(true)}/>
+        }
+      </div>
+          <Link to='/create-article' className='create-article-link'>
+            New Note
+            <MdOutlinePostAdd  className='add-icon'/>
+          </Link>
+          <ul className='category-list'>
+              {sidebarContent && sidebarContent.categories.map(c=>(<Category key={c.id} category={c} />))}
+              <ArtilceLink articles={sidebarContent?.articles || []} />
+          </ul>
+      </aside>
+    </>
   )
 }
