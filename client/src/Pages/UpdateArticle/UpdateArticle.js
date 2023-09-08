085c79a29df4cbc523afa2b34ef7ab590c24ba36
KLociJS <kisznerlorant21@gmail.com>
2023-09-08 16:16:18 +0200

    refactor: Refactor updatearticle to use getarticle hook

--- client/src/Pages/UpdateArticle/UpdateArticle.js
+++ client/src/Pages/UpdateArticle/UpdateArticle.js
@@ -1,28 +1,17 @@
-import { useEffect, useState } from 'react'
-import { getArticleById } from '_Constants/fetchUrl'
-import { useParams } from 'react-router-dom'
 import { RiSave2Line } from 'react-icons/ri'
 import { MarkdownEditor, Modal } from 'Components'
 import UpdateArticleModalContent from './Components/UpdateArticleModal/UpdateArticleModalContent'
+import { useGetArticle } from 'Hooks'
 
 export default function UpdateArticle() {
+    const { 
+        markdown, 
+        setMarkdown, 
+        title,
+        isLoading
+      } = useGetArticle()
 
-    const [markdown, setMarkdown] = useState()
-    const [title, setTitle] = useState()
-
-    const { id } = useParams()
-
-    useEffect(() => {
-        fetch(`${getArticleById}${id}`, { credentials: 'include' })
-            .then(res => res.json())
-            .then(res => {
-                setTitle(res.data.title)
-                setMarkdown(res.data.markdown)
-            })
-            .catch(err => console.log(err))
-    }, [id])
-
-    if (!markdown) return null
+    if (isLoading) return null
 
     return (
         <div className='editor-page-container'>
@@ -31,7 +20,7 @@ export default function UpdateArticle() {
                     <UpdateArticleModalContent markdown={markdown} title={title}/>
                 </Modal>
             </section>
-            <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+            <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown}/>
         </div>
     )
 }
