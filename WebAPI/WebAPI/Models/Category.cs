454deab16b7be563582aa4abbdf25866124411cb
KLociJS <kisznerlorant21@gmail.com>
2023-09-10 14:52:06 +0200

    fix: Fix sidebar content to only display users notes

--- WebAPI/WebAPI/Models/Category.cs
+++ WebAPI/WebAPI/Models/Category.cs
@@ -5,5 +5,7 @@ public class Category
     public Guid Id { get; set; }
     public string Name { get; set; }
     public List<SubCategory> SubCategories { get; set; }
+    public AppUser Author { get; set; }
+    public Guid AuthorId { get; set; }
     public List<Article> Articles { get; set; }
 }
