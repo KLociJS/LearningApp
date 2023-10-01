d71ce4dce79f062aeacdf590719b8ca9f4ef14f3
KLociJS <kisznerlorant21@gmail.com>
2023-10-01 20:09:35 +0200

    fix: Improve article card

--- client/src/Pages/Home/Components/Featured/Components/ArticleCard.js
+++ client/src/Pages/Home/Components/Featured/Components/ArticleCard.js
@@ -1,11 +1,22 @@
+import { Link } from 'react-router-dom'
 import './ArticleCard.css'
+
+const convertDate = (dateStr) => {
+    const date = new Date(dateStr)
+    const year = date.getFullYear()
+    const month = String(date.getMonth()+1).padStart(2,'0')
+    const day = String(date.getDate()).padStart(2,'0')
+
+    return `${year}.${month}.${day}`
+}
 
 export default function ArticleCard({article}) {
   return (
-    <div className='article-card'>
+    <Link to={`/shared-article/${article.id}`} className='article-card'>
         <h2 className='article-card-title'>{article.title}</h2>
         <p className='article-card-description'>{article.description}</p>
-        <p className='article-card-footer'>Created at {article.createdAt} By {article.author}</p>
-    </div>
+        <p className='article-card-footer'>Created at {convertDate(article.createdAt)} By {article.author}</p>
+    </Link>
+
   )
 }
