21afb78b3d357b44f7af0fd2419605c71dc3a454
KLociJS <kisznerlorant21@gmail.com>
2023-10-02 10:36:54 +0200

    feat: Add lazy load for faster homepage load

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 
 import AuthContext from "Context";
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 
 //Stylesheets
 import './GlobalStyle/Index.css'
@@ -24,13 +24,10 @@ import {
     Login,
     SingUp,
     RequesPasswordReset,
-    Users,
-    Articles,
     ConfirmEmail,
-    CreateArticle,
     RequestPasswordChange,
-    UpdateArticle
 } from "Pages"
+
 
 import { RequireRoles, UnauthenticatedRoute } from "Components";
 
@@ -38,24 +35,53 @@ import { checkAuthentication } from "Api";
 
 import ArticleLanding from "Pages/Articles/Components/ArticleLanding/ArticleLanding";
 import Article from "Pages/Articles/Components/Article/Article";
+import UsersSkeleton from "Pages/Users/Components/Skeleton/UsersSkeleton";
 
+const Users = lazy(()=>import("./Pages/Users/Users"))
+const Articles = lazy(()=>import("./Pages/Articles/Articles"))
+const CreateArticle = lazy(()=>import("./Pages/CreateArticle/CreateArticle"))
+const UpdateArticle = lazy(()=>import("./Pages/UpdateArticle/UpdateArticle"))
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route path="shared-article/:id" element={<SharedArticle />} />
+            <Route index element={<Home />} />
             <Route element={<RequireRoles allowedRoles={['User']} />}>
                 <Route path="confirm-email" element={<ConfirmEmail />} />   
-                <Route index element={<Home />} />
-                <Route path="article" element={<Articles />}>
+                <Route 
+                    path="article"
+                    element={
+                        <Suspense>
+                            <Articles fallback={<></>} />
+                        </Suspense>}
+                >
                     <Route index element={<ArticleLanding />} />
                     <Route path=":id" element={<Article />} />
                 </Route>
-                <Route path="create-article" element={<CreateArticle />} />
-                <Route path="update-article/:id" element={<UpdateArticle />} />
+                <Route
+                    path="create-article"
+                    element={
+                        <Suspense fallback={<></>}>
+                            <CreateArticle />
+                        </Suspense>}
+                />
+                <Route
+                    path="update-article/:id"
+                    element={
+                        <Suspense>
+                            <UpdateArticle />
+                        </Suspense>}
+                />
             </Route>
             <Route element={<RequireRoles allowedRoles={['Admin']} />}>
-                <Route path="users" element={<Users />} />
+                <Route 
+                    path="users" 
+                    element={
+                        <Suspense fallback={<UsersSkeleton/>}>
+                            <Users />
+                        </Suspense>}
+                />
             </Route>
             <Route element={<UnauthenticatedRoute />}>
                 <Route path="login" element={<Login />} />
