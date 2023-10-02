4fbda25717676e386380084c2bc08767b525738a
KLociJS <kisznerlorant21@gmail.com>
2023-10-02 12:45:48 +0200

    refactor: Decouple fetch from featured articles

--- client/src/Pages/Home/Components/Featured/Featured.js
+++ client/src/Pages/Home/Components/Featured/Featured.js
@@ -1,21 +1,14 @@
-import { featuredArticle } from '_Constants/fetchUrl'
-import { useEffect, useState } from 'react'
 import ArticleCard from './Components/ArticleCard'
 import './Featured.css'
 import ArticleCardSkeleton from './Components/Components/ArticleCardSkeleton'
+import useGetFeaturedArticles from './Hooks/useGetFeaturedArticles'
 
 export default function Featured() {
 
-    const [articles,setArticles] = useState([])
-    const [isLoading,setIsLoading] = useState(true)
-
-    useEffect(()=>{
-        fetch(featuredArticle)
-        .then(res=>res.json())
-        .then(res=>setArticles(res.data))
-        .catch(err=>console.log(err))
-        .finally(_=>setIsLoading(false))
-    },[])
+    const {
+        articles,
+        isLoading
+    } = useGetFeaturedArticles()
 
     if(isLoading){
         return <ArticleCardSkeleton />
