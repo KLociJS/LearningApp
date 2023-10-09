7bf11c1d1e1110d17bb00ac25dc2c4f5b84c4059
KLociJS <kisznerlorant21@gmail.com>
2023-10-09 15:09:57 +0200

    refactor: Fix typo

--- client/src/Hooks/index.js
+++ client/src/Hooks/index.js
@@ -1,20 +1,31 @@
-import useArticle from './useArticle'
-import useAuth from "./useAuth"
-import useConfirmEmail from "./useConfirmEmail"
-import useGetArticle from "./useGetArticle"
-import useGetSidebarContent from "./useGetSidebarContent"
-import useGetUsers from "./useGetUsers"
-import useLogin from "./useLogin"
-import useLogout from "./useLogout"
-import usePostArticle from "./usePostArticle"
-import useRequesPasswordReset from "./useRequesPasswordReset"
-import useRequestPasswordChange from "./useRequestPasswordChange"
-import useSignUp from "./useSignUp"
-import useUpdateArticle from "./useUpdateArticle"
-import useUpdateRoles from "./useUpdateRoles"
+import useArticle from './useArticle';
+import useAuth from './useAuth';
+import useConfirmEmail from './useConfirmEmail';
+import useGetArticle from './useGetArticle';
+import useGetSidebarContent from './useGetSidebarContent';
+import useGetUsers from './useGetUsers';
+import useLogin from './useLogin';
+import useLogout from './useLogout';
+import usePostArticle from './usePostArticle';
+import useRequestPasswordChange from './useRequestPasswordChange';
+import useRequestPasswordReset from './useRequestPasswordReset';
+import useSignUp from './useSignUp';
+import useUpdateArticle from './useUpdateArticle';
+import useUpdateRoles from './useUpdateRoles';
 
 export {
-    useArticle, useAuth, useConfirmEmail, useGetArticle, useGetSidebarContent, useGetUsers, useLogin, useLogout, usePostArticle, useRequesPasswordReset, useRequestPasswordChange, useSignUp,
-    useUpdateArticle,
-    useUpdateRoles
-}
+  useArticle,
+  useAuth,
+  useConfirmEmail,
+  useGetArticle,
+  useGetSidebarContent,
+  useGetUsers,
+  useLogin,
+  useLogout,
+  usePostArticle,
+  useRequestPasswordChange,
+  useRequestPasswordReset,
+  useSignUp,
+  useUpdateArticle,
+  useUpdateRoles
+};
