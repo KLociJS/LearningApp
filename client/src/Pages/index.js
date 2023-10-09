3bf4368dee54880e2d6f7690ed4f77089a6a6612
KLociJS <kisznerlorant21@gmail.com>
2023-10-09 15:09:09 +0200

    fix: Fix lazy loading

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,29 +1,21 @@
-import Articles from "./Articles/Articles";
-import ConfirmEmail from "./ConfirmEmail/ConfirmEmail";
-import CreateArticle from "./CreateArticle/CreateArticle";
-import Home from "./Home/Home";
-import Login from "./Login/Login";
-import RequestPasswordChange from "./RequestPasswordChange/RequestPasswordChange";
-import RequesPasswordReset from "./ResetPassword/RequesPasswordReset";
-import SharedArticle from "./SharedArticle/SharedArticle";
-import SingUp from "./SignUp/SignUp";
-import UnAuthorized from "./UnAuthorized/UnAuthorized";
-import UpdateArticle from "./UpdateArticle/UpdateArticle.1";
-import Users from "./Users/Users";
-import Layout from "./_Layout/Layout";
+import ConfirmEmail from './ConfirmEmail/ConfirmEmail';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import RequestPasswordChange from './RequestPasswordChange/RequestPasswordChange';
+import RequestPasswordReset from './RequestPasswordReset/RequestPasswordReset';
+import SharedArticle from './SharedArticle/SharedArticle';
+import SingUp from './SignUp/SignUp';
+import UnAuthorized from './UnAuthorized/UnAuthorized';
+import Layout from './_Layout/Layout';
 
 export {
-  Articles,
   ConfirmEmail,
-  CreateArticle,
   Home,
   Layout,
   Login,
-  RequesPasswordReset,
   RequestPasswordChange,
+  RequestPasswordReset,
   SharedArticle,
   SingUp,
-  UnAuthorized,
-  UpdateArticle,
-  Users,
+  UnAuthorized
 };
