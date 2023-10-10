26ceb86f65b752ba745e15fce0bf40e06646b076
KLociJS <kisznerlorant21@gmail.com>
2023-10-10 15:26:58 +0200

    fix: Add stop propagation to delete article modal

--- client/src/Pages/Articles/Components/Article/Components/DeleteArticleModal/DeleteArticleModalContent.js
+++ client/src/Pages/Articles/Components/Article/Components/DeleteArticleModal/DeleteArticleModalContent.js
@@ -1,20 +1,24 @@
-import { useNavigate, useOutletContext } from "react-router-dom"
-import { deleteArticleFetch } from "Api"
+import { deleteArticleFetch } from "Api";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
-export default function DeleteArticleModalConatent({id, setShow}) {
-    const navigate = useNavigate()
-    const { setSidebarContent } = useOutletContext()
+export default function DeleteArticleModalConatent({ id, setShow }) {
+  const navigate = useNavigate();
+  const { setSidebarContent } = useOutletContext();
 
-    const deleteHandler = () =>{
-        deleteArticleFetch(setSidebarContent, id, navigate)
-    }
-    
-    return (
-        <section className='modal'>
-            <h3 className='modal-header warning'>Warning!</h3>
-            <p className='modal-text mb-2'>Article Deletion Is Irreversible!</p>
-            <button className='warning-button' onClick={deleteHandler}>Delete</button>
-            <button className='secondary-button' onClick={()=>setShow(false)}>Cancel</button>
-        </section>
-    )
+  const deleteHandler = () => {
+    deleteArticleFetch(setSidebarContent, id, navigate);
+  };
+
+  return (
+    <section className="modal" onClick={(e) => e.stopPropagation()}>
+      <h3 className="modal-header warning">Warning!</h3>
+      <p className="modal-text mb-2">Article Deletion Is Irreversible!</p>
+      <button className="warning-button" onClick={deleteHandler}>
+        Delete
+      </button>
+      <button className="secondary-button" onClick={() => setShow(false)}>
+        Cancel
+      </button>
+    </section>
+  );
 }
