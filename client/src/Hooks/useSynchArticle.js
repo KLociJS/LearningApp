db7ccb39a778ec1d0203d464ef352d3fe7bbb2e7
KLociJS <kisznerlorant21@gmail.com>
2023-10-10 16:06:54 +0200

    refactor: Add useSynchArticle

---
+++ client/src/Hooks/useSynchArticle.js
@@ -0,0 +1,42 @@
+import { getArticleById } from "_Constants/fetchUrl";
+import { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+
+const initialState = {
+  article: null,
+  isLoading: true,
+  error: null
+};
+
+const articleReducer = (state, action) => {
+  switch (action.type) {
+    case "fetch_article_request": {
+      return { ...state, isLoading: true };
+    }
+    case "fetch_article_success": {
+      return { ...state, isLoading: false, article: action.payload };
+    }
+    case "fetch_article_fail": {
+      return { ...state, isLoading: false, error: "Error. Try again later." };
+    }
+  }
+  throw Error("Unknown action: " + action.type);
+};
+
+export default function useSynchArticle() {
+  const [state, dispatch] = useReducer(articleReducer, initialState);
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch({ type: "fetch_article_request" });
+    fetch(`${getArticleById}${id}`, { credentials: "include" })
+      .then((res) => res.json())
+      .then(({ data }) => {
+        console.log(data);
+        dispatch({ type: "fetch_article_success", payload: data });
+      })
+      .catch((err) => dispatch({ type: "fetch_article_fail" }));
+  }, [id]);
+
+  return { state, dispatch };
+}
