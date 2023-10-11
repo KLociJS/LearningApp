7948bdf7cd7c1058443070c3784aaae70bea3986
KLociJS <kisznerlorant21@gmail.com>
2023-10-11 12:38:36 +0200

    feat: Improve markdown editor

--- client/src/Components/MarkdownEditor/MarkdownEditor.js
+++ client/src/Components/MarkdownEditor/MarkdownEditor.js
@@ -10,6 +10,17 @@ import { IoHelpOutline } from "react-icons/io5";
 import { LuHeading1, LuHeading2, LuHeading3 } from "react-icons/lu";
 import { PiImage } from "react-icons/pi";
 import useScrollSynch from "./Hooks/useScrollSynch";
+import {
+  addCodeBlock,
+  addH1,
+  addH2,
+  addH3,
+  addImage,
+  addLink,
+  addQuote,
+  makeBold,
+  makeItalic
+} from "./Utility/EditorUtility";
 
 export default function MarkdownEditor({ markdown, setMarkdown }) {
   const [showHelp, setShowHelp] = useState(false);
@@ -20,86 +31,48 @@ export default function MarkdownEditor({ markdown, setMarkdown }) {
 
   useScrollSynch(textAreaRef, markdownPreviewRef);
 
-  // Helpers
-  const modifySelected = (ref, element) => {
-    const textArea = ref.current;
-    const textBeforeSelected = textArea.value.substring(0, textArea.selectionStart);
-    const selectedText = textArea.value.substring(textArea.selectionStart, textArea.selectionEnd);
-    const textAfterSelected = textArea.value.substring(textArea.selectionEnd);
-
-    return `${textBeforeSelected}${element}${selectedText}${element}${textAfterSelected}`;
-  };
-
-  const insertAtCurseor = (ref, element) => {
-    const textArea = ref.current;
-    const { selectionStart, selectionEnd } = textArea;
-
-    const textBeforeCursor = textArea.value.substring(0, selectionStart);
-    const textAfterCursor = textArea.value.substring(selectionEnd, textArea.value.length);
-
-    return `${textBeforeCursor}${element}${textAfterCursor}`;
-  };
-
-  const addLink = () => {
-    const newText = insertAtCurseor(textAreaRef, "[title](https://www.example.com)");
-    setMarkdown(newText);
-  };
-
-  const addImage = () => {
-    const newText = insertAtCurseor(textAreaRef, "![alt text](image-link)");
-    setMarkdown(newText);
-  };
-
-  const addQuote = () => {
-    const newText = insertAtCurseor(textAreaRef, ">");
-    setMarkdown(newText);
-  };
-
-  const addCodeBlock = () => {
-    const newText = insertAtCurseor(textAreaRef, "``` lang \n \n ```");
-    setMarkdown(newText);
-  };
-
-  const addH1 = () => {
-    const newText = insertAtCurseor(textAreaRef, "# ");
-    setMarkdown(newText);
-  };
-
-  const addH2 = () => {
-    const newText = insertAtCurseor(textAreaRef, "## ");
-    setMarkdown(newText);
-  };
-
-  const addH3 = () => {
-    const newText = insertAtCurseor(textAreaRef, "### ");
-    setMarkdown(newText);
-  };
-
-  const makeBold = () => {
-    const result = modifySelected(textAreaRef, "**");
-    setMarkdown(result);
-  };
-
-  const makeItealic = () => {
-    const result = modifySelected(textAreaRef, "*");
-    setMarkdown(result);
-  };
-
   return (
     <div className="editor-container">
       <section className="markdown-tools">
-        <BsTypeBold className="markdown-tool-icon" onClick={makeBold} />
-        <BsTypeItalic className="markdown-tool-icon" onClick={makeItealic} />
+        <BsTypeBold
+          className="markdown-tool-icon"
+          onClick={() => makeBold(textAreaRef, setMarkdown)}
+        />
+        <BsTypeItalic
+          className="markdown-tool-icon"
+          onClick={() => makeItalic(textAreaRef, setMarkdown)}
+        />
         <div className="markdown-tool-separator"></div>
-        <LuHeading1 className="markdown-tool-icon" onClick={addH1} />
-        <LuHeading2 className="markdown-tool-icon" onClick={addH2} />
-        <LuHeading3 className="markdown-tool-icon" onClick={addH3} />
+        <LuHeading1
+          className="markdown-tool-icon"
+          onClick={() => addH1(textAreaRef, setMarkdown)}
+        />
+        <LuHeading2
+          className="markdown-tool-icon"
+          onClick={() => addH2(textAreaRef, setMarkdown)}
+        />
+        <LuHeading3
+          className="markdown-tool-icon"
+          onClick={() => addH3(textAreaRef, setMarkdown)}
+        />
         <div className="markdown-tool-separator"></div>
-        <BsCodeSlash className="markdown-tool-icon" onClick={addCodeBlock} />
-        <BsBlockquoteLeft className="markdown-tool-icon" onClick={addQuote} />
+        <BsCodeSlash
+          className="markdown-tool-icon"
+          onClick={() => addCodeBlock(textAreaRef, setMarkdown)}
+        />
+        <BsBlockquoteLeft
+          className="markdown-tool-icon"
+          onClick={() => addQuote(textAreaRef, setMarkdown)}
+        />
         <div className="markdown-tool-separator"></div>
-        <AiOutlineLink className="markdown-tool-icon" onClick={addLink} />
-        <PiImage className="markdown-tool-icon" onClick={addImage} />
+        <AiOutlineLink
+          className="markdown-tool-icon"
+          onClick={() => addLink(textAreaRef, setMarkdown)}
+        />
+        <PiImage
+          className="markdown-tool-icon"
+          onClick={() => addImage(textAreaRef, setMarkdown)}
+        />
         <IoHelpOutline
           className={`markdown-tool-icon last ${showHelp ? "active" : ""}`}
           onClick={() => setShowHelp((s) => !s)}
