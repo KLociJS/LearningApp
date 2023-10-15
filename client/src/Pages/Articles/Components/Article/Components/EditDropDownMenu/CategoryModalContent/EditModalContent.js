729080c031751f9cc61876d41613a8afee792c43
KLociJS <kisznerlorant21@gmail.com>
2023-10-15 17:55:09 +0200

    refactor: Replace use state with use reducer

--- client/src/Pages/Articles/Components/Article/Components/EditDropDownMenu/CategoryModalContent/EditModalContent.js
+++ client/src/Pages/Articles/Components/Article/Components/EditDropDownMenu/CategoryModalContent/EditModalContent.js
@@ -2,14 +2,37 @@ import { Input } from "Components";
 import useUpdateCategory from "../Hooks/useUpdateCategory";
 
 export default function EditModalContent({ setShow }) {
-  const { category, setCategory, subCategory, setSubcategory, handleCategoryUpdate } =
-    useUpdateCategory(setShow);
+  const {
+    state: { category, categoryError, subCategory, fetchError, isDisabled },
+    dispatch,
+    handleCategoryUpdate
+  } = useUpdateCategory(setShow);
+
+  const setCategory = (category) => {
+    dispatch({ type: "set_category", payload: category });
+  };
+
+  const setSubCategory = (subCategory) => {
+    dispatch({ type: "set_subCategory", payload: subCategory });
+  };
 
   return (
     <div className="modal" onClick={(e) => e.stopPropagation()}>
       <h2 className="modal-header">Edit category</h2>
-      <Input label="Category" inputValue={category} setInputValue={setCategory} />
-      <Input label="Subcategory" inputValue={subCategory} setInputValue={setSubcategory} />
+      <Input
+        label="Category"
+        inputValue={category}
+        setInputValue={setCategory}
+        hasError={categoryError}
+        isDisabled={isDisabled}
+      />
+      <Input
+        label="Subcategory"
+        inputValue={subCategory}
+        setInputValue={setSubCategory}
+        isDisabled={isDisabled}
+      />
+      {fetchError ? <p className="error-msg">{fetchError}</p> : null}
       <button className="primary-button" onClick={handleCategoryUpdate}>
         Update
       </button>
--- client/src/Pages/Articles/Components/Article/Components/EditDropDownMenu/Hooks/useUpdateCategory.js
+++ client/src/Pages/Articles/Components/Article/Components/EditDropDownMenu/Hooks/useUpdateCategory.js
@@ -1,22 +1,60 @@
 import useArticleContext from "Hooks/useArticle";
 import useSidebarContent from "Pages/Articles/Hooks/useSidebarContent";
 import { updateCategory } from "_Constants/fetchUrl";
-import { useState } from "react";
+import { useReducer } from "react";
 import { useParams } from "react-router-dom";
+
+const updateCategoryReducer = (state, action) => {
+  switch (action.type) {
+    case "set_category": {
+      return { ...state, category: action.payload, categoryError: null };
+    }
+    case "set_category_error": {
+      return { ...state, categoryError: "Category is required." };
+    }
+    case "set_subCategory": {
+      return { ...state, subCategory: action.payload };
+    }
+    case "fetch_update_error": {
+      return { ...state, fetchError: "Couldn't update category. Try again later" };
+    }
+    case "fetch_update_pending": {
+      return { ...state, isDisabled: true };
+    }
+    case "fetch_update_succeed": {
+      return { ...state, isDisabled: false };
+    }
+  }
+  throw new Error("Invalid action.");
+};
 
 export default function useUpdateCategory(setShow) {
   const { id } = useParams();
   const { setSidebarContent } = useSidebarContent();
-  const { state, dispatch } = useArticleContext();
-  const [category, setCategory] = useState(state.article.category);
-  const [subCategory, setSubcategory] = useState(state.article.subCategory);
+  const { state: articleState, dispatch: articleDispatch } = useArticleContext();
+
+  const initialState = {
+    category: articleState.article.category,
+    categoryError: null,
+    subCategory: articleState.article.subCategory,
+    fetchError: null,
+    isDisabled: false
+  };
+
+  const [state, dispatch] = useReducer(updateCategoryReducer, initialState);
 
   const handleCategoryUpdate = () => {
+    if (state.category.length < 2) {
+      dispatch({ type: "set_category_error" });
+      return;
+    }
+
     const categoryData = {
-      category,
-      subCategory: subCategory ? subCategory : null
+      category: state.category,
+      subCategory: state.subCategory ? state.subCategory : null
     };
 
+    dispatch({ type: "fetch_update_pending" });
     fetch(`${updateCategory}${id}`, {
       method: "PATCH",
       headers: {
@@ -25,19 +63,27 @@ export default function useUpdateCategory(setShow) {
       credentials: "include",
       body: JSON.stringify(categoryData)
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error();
+        }
+      })
       .then((res) => {
         setSidebarContent(res);
-        dispatch({ type: "update_article_category", payload: { ...categoryData } });
+        dispatch({ type: "fetch_update_succeed" });
+        articleDispatch({ type: "update_article_category", payload: { ...categoryData } });
         setShow(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        dispatch({ type: "fetch_update_error" });
+        console.log(err);
+      });
   };
   return {
-    category,
-    setCategory,
-    subCategory,
-    setSubcategory,
+    state,
+    dispatch,
     handleCategoryUpdate
   };
 }
