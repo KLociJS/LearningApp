ac8645a4a81534a602ccb25bfeb8f05e7571e0be
KLociJS <kisznerlorant21@gmail.com>
2023-10-15 19:44:21 +0200

    refactor: Replace use state with use reducer in usePublish

--- client/src/Pages/Articles/Components/Article/Components/PublishDropDownMenu/PublishArticleModalContent/Hooks/usePublishArticle.js
+++ client/src/Pages/Articles/Components/Article/Components/PublishDropDownMenu/PublishArticleModalContent/Hooks/usePublishArticle.js
@@ -1,20 +1,70 @@
 import useArticleContext from "Hooks/useArticle";
 import { publishArticle } from "_Constants/fetchUrl";
-import { useState } from "react";
+import { useReducer } from "react";
 import { useParams } from "react-router-dom";
+
+const publishArticleReducer = (state, action) => {
+  switch (action.type) {
+    case "set_tags": {
+      return { ...state, tagsError: null, tags: action.payload };
+    }
+    case "tags_error": {
+      return { ...state, tagsError: action.payload };
+    }
+    case "set_description": {
+      return { ...state, descriptionError: false, description: action.payload };
+    }
+    case "description_error": {
+      return { ...state, descriptionError: true };
+    }
+    case "post_fetch_pending": {
+      return { ...state, isDisabled: true };
+    }
+    case "post_fetch_succeed": {
+      return { ...state, isDisabled: false };
+    }
+    case "post_fetch_failed": {
+      return {
+        ...state,
+        isDisabled: false,
+        fetchError: "Unable to publish article. Try again."
+      };
+    }
+  }
+  throw new Error("Invalid action.");
+};
+
+const initialState = {
+  tags: [""],
+  tagsError: null,
+  description: "",
+  descriptionError: false,
+  isDisabled: false,
+  fetchError: null
+};
 
 export default function usePublishArticle(setShow) {
   const { id } = useParams();
-  const [description, setDescription] = useState("");
-  const [tags, setTags] = useState("");
-  const { dispatch } = useArticleContext();
+  const { dispatch: articleDispatch } = useArticleContext();
+  const [state, dispatch] = useReducer(publishArticleReducer, initialState);
 
   const publishArticleHandler = () => {
+    if (state.description.length < 100 && description.length > 400) {
+      dispatch({ type: "description_error" });
+      return;
+    }
+
+    if (state.tags.length < 1) {
+      dispatch({ type: "tags_error", payload: "Use at least one tag." });
+      return;
+    }
+
     const publishedArticleDetails = {
-      description,
-      tags: tags.split(",")
+      description: state.description,
+      tags: state.tags
     };
 
+    dispatch({ type: "post_fetch_pending" });
     fetch(`${publishArticle}${id}`, {
       method: "POST",
       headers: {
@@ -23,19 +73,27 @@ export default function usePublishArticle(setShow) {
       credentials: "include",
       body: JSON.stringify(publishedArticleDetails)
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error();
+        }
+      })
       .then(() => {
         setShow(false);
-        dispatch({ type: "publish_article", payload: { ...publishedArticleDetails } });
+        articleDispatch({ type: "publish_article", payload: { ...publishedArticleDetails } });
+        dispatch({ type: "post_fetch_succeed" });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        dispatch({ type: "post_fetch_failed" });
+        console.log(err);
+      });
   };
 
   return {
-    description,
-    setDescription,
-    tags,
-    setTags,
+    state,
+    dispatch,
     publishArticleHandler
   };
 }
