19c8f7cfb468676407fdf7ea9578ed1ca507ac6b
KLociJS <kisznerlorant21@gmail.com>
2023-10-16 09:53:30 +0200

    feat: Add fetch pending, failed indicator to un publish

--- client/src/Pages/Articles/Components/Article/Components/PublishDropDownMenu/PublishUpdateModalContent/Hooks/useUnpublishArticle.js
+++ client/src/Pages/Articles/Components/Article/Components/PublishDropDownMenu/PublishUpdateModalContent/Hooks/useUnpublishArticle.js
@@ -1,21 +1,39 @@
 import useArticleContext from "Hooks/useArticle";
 import { unpublishArticle } from "_Constants/fetchUrl";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function useUnpublishArticle(setShow) {
   const { id } = useParams();
   const { dispatch } = useArticleContext();
+  const [fetchError, setFetchError] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+
   const unpublishHandler = () => {
+    setFetchError(false);
+    setIsDisabled(true);
     fetch(`${unpublishArticle}${id}`, { method: "POST", credentials: "include" })
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error();
+        }
+      })
       .then(() => {
         dispatch({ type: "unpublish_article" });
         setShow(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setFetchError(true);
+        console.log(err);
+      })
+      .finally(() => setIsDisabled(false));
   };
 
   return {
-    unpublishHandler
+    unpublishHandler,
+    fetchError,
+    isDisabled
   };
 }
