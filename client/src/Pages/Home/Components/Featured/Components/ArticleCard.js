88aa2a582c48ad4fa7e53940aefa409ef9998136
KLociJS <kisznerlorant21@gmail.com>
2023-10-26 20:32:43 +0200

    style: Improve article card date on home page

--- client/src/Pages/Home/Components/Featured/Components/ArticleCard.js
+++ client/src/Pages/Home/Components/Featured/Components/ArticleCard.js
@@ -1,14 +1,22 @@
-import { Link } from 'react-router-dom'
-import './ArticleCard.css'
-import convertDate from 'Utility/convertDate'
+import convertDate from "Utility/convertDate";
+import { Link, useNavigate } from "react-router-dom";
+import "./ArticleCard.css";
 
-export default function ArticleCard({article}) {
+export default function ArticleCard({ article }) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/shared-article/${article.id}`);
+  };
   return (
-    <Link to={`/shared-article/${article.id}`} className='article-card'>
-        <h2 className='article-card-title'>{article.title}</h2>
-        <p className='article-card-description'>{article.description}</p>
-        <p className='article-card-footer'>Created at {convertDate(article.createdAt)} By {article.author}</p>
-    </Link>
-
-  )
+    <div onClick={handleClick} className="article-card">
+      <h2 className="article-card-title">{article.title}</h2>
+      <p className="article-card-description">{article.description}</p>
+      <p className="article-card-footer" onClick={(e) => e.stopPropagation()}>
+        {convertDate(article.createdAt)} By{" "}
+        <Link to={`profile/${article.author}`} className="author-link">
+          {article.author}
+        </Link>
+      </p>
+    </div>
+  );
 }
