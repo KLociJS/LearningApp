4e54b93850cd0fb1dc5f3465256b5d782db5262f
KLociJS <kisznerlorant21@gmail.com>
2023-10-31 17:07:24 +0100

    refactor: Refactor article card

---
+++ client/src/Components/Featured/Components/ArticleCard/Components/ArticleAuthorData/ArticleAuthorData.js
@@ -0,0 +1,30 @@
+import defaultProfilePicture from "Pages/Profile/Components/ProfileData/ProfilePicutre/Default/default-profile.jpg";
+import convertDate from "Utility/convertDate";
+import { getProfilePicture } from "_Constants/fetchUrl";
+import { Link } from "react-router-dom";
+
+export default function ArticleAuthorData({ article }) {
+  const profilePictureUrl = article.authorProfilePicture ? article.authorProfilePicture : null;
+
+  return (
+    <div className="article-card-author-container">
+      <img
+        src={
+          profilePictureUrl
+            ? `${getProfilePicture}${article.authorProfilePicture}`
+            : defaultProfilePicture
+        }
+        alt="author profile picture"
+        className="article-card-profile-picture"
+      />
+      <div className="article-card-author-data">
+        <Link to={`/profile/${article.author}`} className="author-link">
+          {article.author}
+        </Link>
+        <p className="article-card-date" onClick={(e) => e.stopPropagation()}>
+          {convertDate(article.createdAt)}
+        </p>
+      </div>
+    </div>
+  );
+}
