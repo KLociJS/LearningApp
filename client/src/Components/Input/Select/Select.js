bbf075e662c087ede6996bab9c692c6163f948a2
KLociJS <kisznerlorant21@gmail.com>
2023-11-02 13:22:33 +0100

    feat: Implement report components in react

---
+++ client/src/Components/Input/Select/Select.js
@@ -0,0 +1,14 @@
+import "./Select.css";
+
+export default function Select() {
+  return (
+    <>
+      <select className="select-input">
+        <option value="">Choose an option</option>
+        <option value="">Choose an option</option>
+        <option value="">Choose an option</option>
+        <option value="">Choose an option</option>
+      </select>
+    </>
+  );
+}
--- client/src/Pages/SharedArticle/SharedArticle.js
+++ client/src/Pages/SharedArticle/SharedArticle.js
@@ -3,13 +3,15 @@ import SharedArticleSkeleton from "./Components/SharedArticleSkeleton";
 import useGetSharedArticle from "./Hooks/useGetSharedArticle";
 import useGetSharedSidebarContent from "./Hooks/useGetSharedSidebarContent";
 
-import { Sidebar } from "Components";
+import { Modal, ModalTriggerElement, Sidebar } from "Components";
 import MarkdownPreview from "Components/MarkdownEditor/MarkdownPreview/MarkdownPreview";
 import convertDate from "Utility/convertDate";
 import { Link } from "react-router-dom";
 import "../Articles/Articles.css";
 import useGetHeadingIds from "./Components/Hooks/useGetHeadingIds";
+import ReportArticleModalContent from "./Components/ReportArticleModalContent/ReportArticleModalContent";
 import TableOfContents from "./Components/TableOfContents";
+import "./SharedArticle.css";
 
 export default function SharedArticle() {
   const { article, isArticleLoading } = useGetSharedArticle();
@@ -40,6 +42,12 @@ export default function SharedArticle() {
                   {article.updatedAt != null ? convertDate(article.updatedAt) : null}
                 </p>
               </div>
+              <Modal
+                triggerElement={
+                  <ModalTriggerElement text="report" className="report-article-button" />
+                }
+                modalContent={<ReportArticleModalContent />}
+              />
             </section>
             <MarkdownPreview markdown={article.markdown} setHeadings={setHeadings} />
           </>
