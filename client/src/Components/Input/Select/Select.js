a430f25f466f751d2da711febc40dea911d80920
KLociJS <kisznerlorant21@gmail.com>
2023-11-02 19:02:20 +0100

    feat: Implement select Input

--- client/src/Components/Input/Select/Select.js
+++ client/src/Components/Input/Select/Select.js
@@ -1,14 +1,25 @@
 import "./Select.css";
 
-export default function Select() {
+export default function Select({ error, setError, selectValue, setSelectValue, isDisabled }) {
+  const handleChange = (e) => {
+    setSelectValue(e.target.value);
+    setError(false);
+  };
+
   return (
-    <>
-      <select className="select-input">
-        <option value="">Choose an option</option>
-        <option value="">Choose an option</option>
-        <option value="">Choose an option</option>
-        <option value="">Choose an option</option>
+    <div>
+      <select
+        disabled={isDisabled}
+        value={selectValue}
+        onChange={handleChange}
+        className={`select-input ${error ? "error" : ""}`}>
+        <option value=""> --Choose an option-- </option>
+        <option value="PhishingLink">Phishing link</option>
+        <option value="InappropriateContent">Inappropriate content</option>
+        <option value="MissInformation">Miss information</option>
+        <option value="Other">Other</option>
       </select>
-    </>
+      {error ? <p className="error-msg">Please choose an option.</p> : null}
+    </div>
   );
 }
